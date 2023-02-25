8ec1904a48f2ca4b52033a483c9d2aaf5abb3735
iosokolov <i.sokolov@trafficwave.kz>
2023-02-25 21:48:23 +0600

    some

---
+++ airflow/main.py
@@ -0,0 +1,22 @@
+import asyncio
+import json
+
+from sanic import Sanic
+import sanic
+
+
+app = Sanic("my_first_app")
+
+
+@app.post('/search')
+async def post_search(request):
+    await asyncio.sleep(60)
+    with open('response_b.json') as f:
+        text = f.read()
+
+    data = json.loads(text)
+    return sanic.response.json(data)
+
+
+if __name__ == '__main__':
+    app.run(port=9000)
